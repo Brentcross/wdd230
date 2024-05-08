d703d658cff4fad1b0d77b12bbe2d70fb827979e
Brent Cross <crossm.brent@gmail.com>
2024-05-08 16:38:22 -0600

    completed week 03 - team activity, read to present

---
+++ scripts/visitCounter.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const visitsDisplay = document.querySelector(".visits");
+    let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+
+    if (numVisits !== 0) {
+        visitsDisplay.textContent = numVisits;
+    } else {
+        visitsDisplay.textContent = `Welcome to your first visit!`;
+    }
+
+    numVisits++;
+    localStorage.setItem("numVisits-ls", numVisits);
+});
