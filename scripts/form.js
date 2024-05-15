79ff9812aa6f2ea46084ede030b6eb2911bd4a9c
Brent Cross <crossm.brent@gmail.com>
2024-05-15 17:23:10 -0600

    finished team work for this week

---
+++ scripts/form.js
@@ -0,0 +1,19 @@
+document.getElementById('currentYear').textContent = new Date().getFullYear();
+document.getElementById('lastModified').textContent = `Last Updated: ${document.lastModified}`;
+
+function updateRatingValue(value) {
+    document.getElementById('ratingValue').textContent = value;
+}
+
+function checkPasswords() {
+    const password = document.getElementById('password');
+    const confirm_password = document.getElementById('confirm_password');
+    if (password.value !== confirm_password.value) {
+        alert('Passwords do not match.');
+        password.value = '';
+        confirm_password.value = '';
+        password.focus();
+    }
+}
+
+document.getElementById('confirm_password').addEventListener('blur', checkPasswords);
